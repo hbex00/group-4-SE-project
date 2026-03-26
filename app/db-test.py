1b8fb1e399fc42b6f36f8e0b1222f895c3a3f661
Hugo <hugobex@live.com>
2026-03-26 15:54:48 +0100

    modifiy now able to render html for flask

--- app/db-test.py
+++ app/db-test.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 @app.route('/')
 def home():
