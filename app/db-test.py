831a0a096b84abb93d866dfe375ae61a3ffac96d
Hugo <hugobex@live.com>
2026-03-26 11:49:10 +0100

    added db-test it creates a empty instenc of a db

---
+++ app/db-test.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# creates sqlAlchemy instance
+db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+    with app.app_context():  # Needed for DB operations
+        db.create_all()      # Creates the database and tables
+    app.run(debug=True)
