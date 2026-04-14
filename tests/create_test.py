22456aa838f1eaebf81d25538a53ed4e2f0fcae5
Hugo <hugobex@live.com>
2026-04-14 11:26:52 +0200

    added a seconde unittest for create recepie

--- tests/create_test.py
+++ tests/create_test.py
@@ -3,11 +3,16 @@ from app.routes.create import create_recepie
 
 class Testcreat_recepie(unittest.TestCase):
 
-    def test_upper(self):
+    def test_create_one_recepie(self):
         result = create_recepie("Köttbullar","goda kötbullar", 2)
         self.assertEqual(result.recipe_title,"Köttbullar")
         self.assertEqual(result.description,"goda kötbullar")
         self.assertEqual(result.user_id, 2)
-
+    def test_create_two_recepies(self):
+        result = create_recepie("Hamburgare","goda Hamburgare", 3)
+        self.assertEqual(result.recipe_title,"Hamburgare")
+        self.assertEqual(result.description,"goda Hamburgare")
+        self.assertEqual(result.user_id, 3)
+        
 if __name__ == '__main__':
     unittest.main()
