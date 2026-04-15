6b244161216234a38bcdc5377d39016f02266890
BjarneOS <SvBj23mw@student.ju.se>
2026-04-15 16:14:42 +0200

    added profile button logic

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, request, redirect, Blueprint
+from flask import Flask, render_template, request, redirect, Blueprint, session
+from database.db import db
+from app.services.models import User
+from app.services.models import Recipe
 
 userpage_bp = Blueprint("userpage", __name__)
 
 @userpage_bp.route('/user',methods = ['POST','GET'])
 def userpage():
     if request.method == 'POST':
+        if 'username' in session: 
+            profile_session = session['username']
+            profile_user_id = User.query.filter_by(email=profile_session)
+            user_recipes = Recipe.query.filter_by(user_id=profile_user_id)
         return redirect('/') # Nothing to post yet so... Back to homepage you go!    
     else:
         return render_template('userpage.html')
--- static/JS/topbar.js
+++ static/JS/topbar.js
@@ -17,6 +17,16 @@ user_icon.addEventListener("click", () => {
     user.submit();
 });
 
+
+const profile_button = document.getElementById("profile_button");
+
+const profile_form = document.getElementById("profile");
+
+profile_button.addEventListener("click", () => {
+    profile_form.submit();
+});
+
+
 const logout_button = document.getElementById("logout_button");
 
 const logout_form = document.getElementById("logout");
