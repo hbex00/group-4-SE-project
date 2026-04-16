c94937f7cbb663d37dc3fbc316ec056f3c9ea57a
Bjarne <svbj23mw@student.ju.se>
2026-04-16 16:59:07 +0200

    adjust user recipe-list logic

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, Blueprint, session, flash, get_template_attribute
-from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 from database.db import db
 from app.services.models import User
 from app.services.models import Recipe
@@ -11,15 +11,14 @@ def userpage():
     if request.method == 'POST':
         return redirect('/') # Nothing Post-able added yet! To the homepage with thee!
     else:
-        if 'username' in session:
-            profile_session = session['username']
-            profile_user : User = User.query.filter_by(email=profile_session).first()
-            session['name'] = profile_user.name
+        if 'id' in session:
+            profile_id = session['id']
+            profile_user : User = User.query.filter_by(id=profile_id).first()
+            session['first_name'] = profile_user.name
             if not profile_user.last_name == "":
-                session['surname'] = profile_user.last_name
+                session['last_name'] = profile_user.last_name
             else:
-                if 'surname' in session:
-                    session.pop('surname',None)
+                session.pop('last_name',None)
 
         return render_template('userpage.html')
     
@@ -35,28 +34,12 @@ def recipes():
     if request.method == 'POST':
         return redirect('/') # Nothing Post-able added yet! To the homepage with thee!
     else:
-        if not 'username' in session:
+        if not 'id' in session:
             flash('You need to log in to view your recipes!',category='error')
         else:
             try:
-                user : User = db.session.execute(select(User).where(User.email==session['username'])).first()
-                recipe_list = db.session.execute(select(Recipe.recipe_title).where(Recipe.user_id==user.id)).fetchall()
-                if not recipe_list:
-                    flash("I could not find any recipes!",category='error')
-                else:                           
-                    template_recipes = get_template_attribute('userpage.html', 'recipe_list')
-                    return template_recipes(recipe_list)
+                user = User.query.get(session.get('id'))
+                return render_template('userpage.html',user=user,show_recipes=True)
             except AttributeError as err:
-                flash("It seems you do not have any recipes!",category='error')
+                flash("Attribute Error!>" + str(err),category='error')
         return render_template('userpage.html')
-    
-
-
-    
-def view():
-    id = request.args.get('recipe_id', type = int)
-    recipe = Recipe.query.get(id)
-    if not recipe:
-        return redirect('/')
-    else:
-        return render_template('viewrecipe.html', recipe=recipe)
