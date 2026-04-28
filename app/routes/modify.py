94c5ded82e2de998d35925e19261653586095227
William Klasson <irodoot@gmail.com>
2026-04-28 18:30:26 +0200

    Add backend for modifying tags on a recipe

--- app/routes/modify.py
+++ app/routes/modify.py
@@ -16,9 +16,13 @@ def modify():
         recipe.recipe_title = request.form['title'] 
         recipe.description = request.form['description']
         recipe.portions = request.form['portions']
-    # we delete all the lists of ingredents and steps 
+
+        tag_list = request.form.getlist('tag[]')
+
+    # we delete all the lists of ingredents, steps and tags
         Ingredient.query.filter_by(recipe_id = id).delete()
         Step.query.filter_by(recipe_id = id).delete()
+        RecipeTag.query.filter_by(recipe_id= id).delete()
         db.session.commit()
 
         ingredients = zip(
@@ -32,6 +36,15 @@ def modify():
     # then uses the functions to add them back in 
         ingredients_add(ingredients, id)
         steps_add(steps, id)
+
+        for tags in tag_list:
+            if ':' in tags:
+                t = tags.split(':')
+                found_tag = Tag.query.filter_by(category = t[0].strip(), unit = t[1].strip()).first()
+                print(found_tag)
+                if found_tag:
+                    tag_add(id, found_tag.id)
+
 
         return redirect('/viewrecipe')
     
